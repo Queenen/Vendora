3648395822ebd4d25e6e3a42493ef28bd3a9928b
Annette B <114480324+Queenen@users.noreply.github.com>
2024-04-02 17:30:14 +0200

    Create nav component

---
+++ src/components/nav/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Nav.module.css";
+import Cart from "../cart/";
+
+function Nav() {
+  const itemCount = 1;
+
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.topNav}>
+        <Link to="/home" className={styles.navLogo}>
+          <img
+            src={`${process.env.PUBLIC_URL}/images/vendora-logo.png`}
+            alt="Logo"
+          />
+        </Link>
+        <div className={styles.navLinks}>
+          <Link to="/about" className={styles.navLink}>
+            About
+          </Link>
+          <Link to="/contact" className={styles.navLink}>
+            Contact
+          </Link>
+        </div>
+        <Cart itemCount={itemCount} />
+      </div>
+      <input
+        className={styles.searchBar}
+        type="text"
+        placeholder="Search for products ..."
+      />
+    </nav>
+  );
+}
+
+export default Nav;
