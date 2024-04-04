581082af0da246b46d377e03676e805825eba907
Annette B <114480324+Queenen@users.noreply.github.com>
2024-04-04 21:27:28 +0200

    Update useCart.jsx

--- src/components/cart/useCart.jsx
+++ src/components/cart/useCart.jsx
@@ -5,7 +5,6 @@ function useCart() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    // Load cart from local storage on component mount
     const storedCart = JSON.parse(localStorage.getItem("cart"));
     if (storedCart) {
       setCart(storedCart);
@@ -16,16 +15,13 @@ function useCart() {
   }, []);
 
   useEffect(() => {
-    // Save cart to local storage whenever it changes
     localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
 
   const addToCart = (product) => {
-    // Check if the product already exists in the cart
     const existingItemIndex = cart.findIndex((item) => item.id === product.id);
 
     if (existingItemIndex !== -1) {
-      // If the product already exists, update the quantity
       const updatedCart = [...cart];
       updatedCart[existingItemIndex] = {
         ...updatedCart[existingItemIndex],
@@ -33,11 +29,9 @@ function useCart() {
       };
       setCart(updatedCart);
     } else {
-      // If the product is not in the cart, add it
       setCart([...cart, { ...product, quantity: 1 }]);
     }
 
-    // Increment itemCount
     setItemCount((prevCount) => prevCount + 1);
   };
 
