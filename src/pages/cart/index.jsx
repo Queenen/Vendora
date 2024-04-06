81e2f22aea2540d08850ce9a1f8c288f317c614a
Annette B <114480324+Queenen@users.noreply.github.com>
2024-04-06 14:57:04 +0200

    Update index.jsx

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,5 +1,39 @@
+//import styles from "./CartPage.module.css";
+import CartItems from "./CartItems";
+import Button from "../../components/button/Button";
+import styles from "./CartPage.module.css";
+
 function CartPage() {
-  return <></>;
+  return (
+    <>
+      <div className="container-fluid p-3 py-5" id={styles.cartPage}>
+        <CartItems />
+        <section id="calculateShipping" className="row gap-4 py-5">
+          <div className="d-flex flex-column gap-1">
+            {" "}
+            <label htmlFor="country">Country</label>
+            <select name="country" className="p-2">
+              <option value="Norway">Norway</option>
+            </select>
+          </div>
+          <div className="d-flex flex-column gap-1">
+            {" "}
+            <label htmlFor="shipping">Shipping</label>
+            <select name="shipping" className="p-2">
+              <option value="Standard shipping" selected>
+                Standard Shipping - FREE
+              </option>
+            </select>
+          </div>
+          <div className="d-flex justify-content-between ">
+            <p className="fw-bold">Total</p>
+            <p>$ totalPrice</p>
+          </div>
+        </section>
+        <Button variant="rounded">Next Step</Button>
+      </div>
+    </>
+  );
 }
 
 export default CartPage;
