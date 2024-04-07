3c95acc4a0bcabde1f62a7e926e71c602194ff0e
Annette B <114480324+Queenen@users.noreply.github.com>
2024-04-07 20:00:11 +0200

    Update index.jsx

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,14 +4,14 @@ import styles from "./Footer.module.css";
 
 function Footer() {
   return (
-    <footer className={styles.footer}>
-      <Link to="/" className={styles.footerLogo}>
+    <footer className="p-4 pb-1 px-sm-5 d-flex flex-column bg-whitesmoke">
+      <Link to="/" className={`ms-auto d-block ${styles.footerLogo}`}>
         <img
           src={`${process.env.PUBLIC_URL}/images/vendora-logo.png`}
           alt="Logo"
         />
       </Link>
-      <p className={styles.footerCopyright}>
+      <p className="text-center fst-italic mt-5 pt-5">
         © Vendora 2024. All rights reserved
       </p>
     </footer>
