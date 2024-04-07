6075039cbe92a54c9be03ce802f6f4acc15a5d95
Annette B <114480324+Queenen@users.noreply.github.com>
2024-04-07 20:00:13 +0200

    Update index.jsx

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -8,25 +8,27 @@ function Nav({ products }) {
   const itemCount = 1;
 
   return (
-    <nav className={styles.nav}>
-      <div className={styles.topNav}>
+    <nav className="d-flex flex-column bg-whitesmoke p-4 px-sm-5">
+      <div className="d-flex justify-content-between align-items-center mb-4">
         <Link to="/" className={styles.navLogo}>
           <img
             src={`${process.env.PUBLIC_URL}/images/vendora-logo.png`}
             alt="Logo"
           />
         </Link>
-        <div className={styles.navLinks}>
+        <div
+          className={`d-flex gap-3 gap-md-5 align-items-center ms-auto fs-5`}
+        >
           <Link to="/about" className={styles.navLink}>
             About
           </Link>
           <Link to="/contact" className={styles.navLink}>
             Contact
           </Link>
+          <Link to="/cart">
+            <Cart itemCount={itemCount} />
+          </Link>
         </div>
-        <Link to="/cart">
-          <Cart itemCount={itemCount} />
-        </Link>
       </div>
       <SearchBar products={products} />
     </nav>
