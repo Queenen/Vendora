9fdc7372c49854c3ff379ded37b5ac6d3a51e62f
Annette B <114480324+Queenen@users.noreply.github.com>
2024-04-07 21:58:28 +0200

    Update index.jsx

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -6,32 +6,51 @@ import { fetchAllProducts } from "../../services/fetchProducts";
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [error, setError] = useState(""); // State to track the error message
   const navigate = useNavigate();
 
   useEffect(() => {
     fetchAllProducts()
-      .then((data) => setProducts(data.data))
-      .catch((error) => console.error("Fetching products failed", error));
+      .then((data) => {
+        // Ensure data is not null or undefined
+        if (data && data.data) {
+          setProducts(data.data);
+          setError(""); // Reset error message upon successful data fetch
+        } else {
+          // Handle case where data is not in expected format or empty
+          throw new Error("No products found");
+        }
+      })
+      .catch((error) => {
+        console.error("Fetching products failed", error);
+        setError("Failed to load products. Please try again later."); // Set an error message
+      });
   }, []);
 
   return (
     <div className={styles.productsContainer}>
       <h1>Our Products</h1>
-      <div className={styles.productsMasonry}>
-        {products.map((product) => (
-          <div
-            className={styles.productCard}
-            key={product.id}
-            onClick={() => navigate(`/product?id=${product.id}`)}
-          >
-            <img
-              src={product.image.url}
-              alt={product.title || "Product Image"}
-            />
-            <Button variant="halfround">View Product</Button>
-          </div>
-        ))}
-      </div>
+      {error ? ( // Check if there is an error
+        <div className={styles.errorMessage}>
+          {error} {/* Display the error message */}
+        </div>
+      ) : (
+        <div className={styles.productsMasonry}>
+          {products.map((product) => (
+            <div
+              className={styles.productCard}
+              key={product.id}
+              onClick={() => navigate(`/product?id=${product.id}`)}
+            >
+              <img
+                src={product.image.url}
+                alt={product.title || "Product Image"}
+              />
+              <Button variant="halfround">View Product</Button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
